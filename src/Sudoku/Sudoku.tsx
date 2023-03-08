c443d8cc1ac46634dd20b168586d0c5ecbc2c57b
Suraj Jaiswal <96817675+IlIIIlIIIlIlIlI@users.noreply.github.com>
2023-03-08 20:39:27 +0530

    npm install axios

--- src/Sudoku/Sudoku.tsx
+++ src/Sudoku/Sudoku.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import axios from 'axios';
 
 function Sudoku() {
+  axios
+    .get('https://sudoku-puzzle-9x9-presolved-production.up.railway.app/sudoku')
+    .then((value) => console.log('value', value));
   return <div>Sudoku</div>;
 }
 
