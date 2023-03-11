2215400b24f19b1510f6a3ee3d51bfb09fbb81bc
Suraj Jaiswal <96817675+IlIIIlIIIlIlIlI@users.noreply.github.com>
2023-03-11 20:17:13 +0530

    integration of lscss obj

---
+++ src/Input/Input.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import scssObj from './_Input.scss';
+
+function Input() {
+  return <div className={`${scssObj.baseClass}__input`}>Input</div>;
+}
+
+export default Input;
--- src/Sudoku/Sudoku.tsx
+++ src/Sudoku/Sudoku.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import axios from 'axios';
+import Input from '../Input';
 
 function Sudoku() {
   axios
     .get('https://sudoku-puzzle-9x9-presolved-production.up.railway.app/sudoku')
     .then((value) => console.log('value', value));
-  return <div>Sudoku</div>;
+  return (
+    <div>
+      <Input />
+    </div>
+  );
 }
 
 export default Sudoku;
