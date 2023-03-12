e9ba1d3f4794bff0c373f3e78a010fa020f7d6b3
Suraj Jaiswal <96817675+IlIIIlIIIlIlIlI@users.noreply.github.com>
2023-03-12 12:58:26 +0530

    add selectors

---
+++ src/Redux/Selector.ts
@@ -0,0 +1,9 @@
+import { SudokuSuperState } from '../store';
+
+export const selectSudokuState = (state: SudokuSuperState) => state.sudoku;
+
+export const selectPuzzle = (state: SudokuSuperState) =>
+  selectSudokuState(state).puzzle;
+
+export const selectSolution = (state: SudokuSuperState) =>
+  selectSudokuState(state).solution;
